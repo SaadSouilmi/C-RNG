e4de7a59535978aa5820111a46a700fc4d2dd5f2
Saad Souilmi <souilmi.saad00@gmail.com>
2025-04-20 13:28:34 +0200

    Added a help message

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "state.h"
 #include "ran.h"
 
-
+void print_help(const char *prog_name);
 void parse_args(int argc, char* argv[], uint64_t *seed, int *provided_seed, uint32_t *n);
 void sample(Ran *rng, uint32_t n);
     
@@ -39,18 +39,31 @@ void parse_args(int argc, char* argv[], uint64_t *seed, int *provided_seed, uint
     *provided_seed = 0;
     *n = 1;
 
-    for (int i=1; i < argc - 1; i++)
+    for (int i=1; i < argc; i++)
     {
-        if (strcmp(argv[i], "--seed") == 0)
+        if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") ==0)
+        {
+            print_help(argv[0]);
+            exit(0);
+        }
+        if (strcmp(argv[i], "--seed") == 0 && i + 1 < argc)
         {
             *seed = strtoull(argv[i+1], NULL, 10);
             *provided_seed = 1;
             i++;
-        } else if (strcmp(argv[i], "-n") == 0) {
+        } else if (strcmp(argv[i], "-n") == 0 && i + 1 < argc) {
             *n = strtoul(argv[i+1], NULL, 10);
             i++;
         }
     }
+}
+
+void print_help(const char *prog_name)
+{
+    printf("Usage: %s [--seed <uint64_t>] [-n <uint32_t>] [--help]\n", prog_name);
+    printf("  --seed <uint64_t>    Set the seed for the RNG (default: 1337)\n");
+    printf("  -n <uint32_t>        Number of random numbers to generate (default: 1)\n");
+    printf("  --help or -h         Display this help message\n");
 }
 
 void sample(Ran *rng, uint32_t n)
